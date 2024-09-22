facdbda7d0604d395a30327ed67bb81980c1b63c
MeidKoi <dshinotanrid@gmail.com>
2024-09-22 14:27:30 +0300

    editconfig

--- MnogoLibAPI/obj/Debug/net6.0/MnogoLibAPI.AssemblyInfo.cs
+++ MnogoLibAPI/obj/Debug/net6.0/MnogoLibAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MnogoLibAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05b0c93eb0fd352cfd6d17fac21c506f54962a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982be1137d96852ae5a6edc8bc572738ba231b54")]
 [assembly: System.Reflection.AssemblyProductAttribute("MnogoLibAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MnogoLibAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
