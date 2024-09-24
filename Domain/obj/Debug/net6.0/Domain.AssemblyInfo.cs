5e7713dc2679a97d8d0744a0095970d59acc76f5
MeidKoi <dshinotanrid@gmail.com>
2024-09-24 12:53:20 +0300

    Heeeeelp

--- Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net6.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d09eeb4775258d06afd3f8e931c714bdb7f155f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6437feaa181a35ec264f1aa96f54482dd3e68c90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
