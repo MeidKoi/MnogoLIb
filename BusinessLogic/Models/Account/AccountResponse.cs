198b111f7af9345fb33195ae35611ab75e4d7c37
MeidKoi <dshinotanrid@gmail.com>
2024-12-13 19:51:12 +0300

    Есть два порта. На какой сам сядешь, на друго дотнет посадишь

--- BusinessLogic/Models/Account/AccountResponse.cs
+++ BusinessLogic/Models/Account/AccountResponse.cs
@@ -2,14 +2,13 @@ namespace BusinessLogic.Models.Accounts
 {
     public class AccountResponse
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Role { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime? Updated { get; set; }
+        public int IdUser { get; set; }
+        public string EmailUser { get; set; } = null!;
+        public string PasswordUser { get; set; } = null!;
+        public string NicknameUser { get; set; } = null!;
+        public int IdRole { get; set; }
+        public DateTime CreatedTime { get; set; }
+         public DateTime LastUpdateTime { get; set; }
         public bool IsVerified { get; set; }
     }
 }
